f44979c9eeaecf14ad9f4d2ea6e82e71239287d5
Breno Oliveira <breno26@gmail.com>
2015-08-19 17:13:21 -0300

    add skeleton for coupon

---
+++ lib/moip-assinaturas/coupon.rb
@@ -0,0 +1,22 @@
+module Moip::Assinaturas
+  class Invoice
+
+    class << self
+
+      def list(opts={})
+      end
+
+      def details(id, opts={})
+      end
+
+      def create(coupon, opts={})
+      end
+
+      def update(coupon, opts={})
+      end
+
+      def delete(coupon, opts={})
+      end
+    end
+  end
+end
