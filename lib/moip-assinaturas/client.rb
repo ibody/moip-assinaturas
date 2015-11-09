089a498be968267fe4d470c21e7c2f42c518d65c
Guilherme Sipoloni <guilherme.sipoloni@moip.com.br>
2015-11-09 10:43:09 -0200

    retirando debug logs

--- lib/moip-assinaturas/client.rb
+++ lib/moip-assinaturas/client.rb
@@ -85,11 +85,6 @@ module Moip::Assinaturas
       def list_subscriptions(opts={})
         prepare_options(opts, { headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' } })
         
-        puts "<<<<<<<<< #{opts.inspect}" 
-        puts "$$$$$$$$$$$$$ Tem parametro #{opts.include?('query_params')}"
-
-        puts "%%%%%%%%%%5 URI= #{"/subscriptions#{opts['query_params']}"}"
-
         return peform_action!(:get, "/subscriptions", opts) unless opts.include?('query_params')
         return peform_action!(:get, "/subscriptions#{opts['query_params']}", opts) if opts.include?('query_params')
       end
